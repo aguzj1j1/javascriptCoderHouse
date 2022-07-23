a6fc08c49fd7cf5037b264b3fa21a7653949a5db
agustingonzalez660 <agustingonzalez660@gmail.com>
2022-07-23 12:41:53 -0300

    agrego fetch

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ class Cart {
         debugger;
         this.carrito = this.getProductosLocalStorage() || [];
     }
-    addCart(id, cantidad, precio, producto, tieneDescuento,imagenes,stock) {
+    addCart(id, cantidad, precio, producto, tieneDescuento, imagenes, stock) {
         var productos = this.getProductosLocalStorage() || [];
         debugger;
         if (productos.filter(actual => actual.idProducto == id).length > 0) {
-            if (this.validateStock(id)){
+            if (this.validateStock(id)) {
                 this.addQuantity(id);
-            }else{
+            } else {
                 Swal.fire({
                     title: 'No hay más stock del producto',
                     background: ' #8a1538',
                     color: 'white',
-                    
+
                     icon: 'warning',
                     imageWidth: 600,
                     imageHeight: 200,
@@ -23,7 +23,7 @@ class Cart {
                 })
                 return false;
             }
-            
+
         } else {
             var descuentoAplicar, descuentoAplicado = 0;
             if (this.validateParam(cantidad, precio, producto).length > 0) {
@@ -34,15 +34,15 @@ class Cart {
                 descuentoAplicar = Number(prompt("ingrese valor del descuento"));
                 descuentoAplicado = this.calculateDiscount(this.descuento, precio, cantidad);
             }
-            let object ={
+            let object = {
                 idProducto: id,
                 producto: producto,
                 precio: precio,
                 cantidad: cantidad,
                 descuento: descuentoAplicar,
                 precioConDescuento: descuentoAplicado,
-                imagenes:imagenes,
-                stock:stock
+                imagenes: imagenes,
+                stock: stock
             }
             this.carrito.push(object)
             localStorage.setItem("carrito", JSON.stringify(this.carrito));
@@ -86,54 +86,38 @@ class Cart {
         return false;
     }
 
-    validateStock(id){
-       var producto = this.carrito.filter((item) => item.idProducto== id)[0]
-        return producto.stock> producto.cantidad;
+    validateStock(id) {
+        var producto = this.carrito.filter((item) => item.idProducto == id)[0]
+        return producto.stock > producto.cantidad;
     }
 
     getProductosLocalStorage() {
         let productosLocalStorage = JSON.parse(localStorage.getItem("carrito")) || [];
         console.log(productosLocalStorage)
         let productos = []
-            for (const iterator of productosLocalStorage) {
-                    productos.push(iterator);
-            }
+        for (const iterator of productosLocalStorage) {
+            productos.push(iterator);
+        }
         return productos;
     }
 }
-let productos = [{
-    idProducto: 1,
-    cantidad: 1,
-    precio: 30,
-    producto: 'Dermaglos Crema hidratante día',
-    descuento: false,
-    stock: 3,
-    imagenes:'../imgs/7793742002915.jpg'
-}, {
-    idProducto: 2,
-    cantidad: 1,
-    precio: 5,
-    producto: 'Dermaglos Crema hidratante Noche',
-    descuento: false,
-    stock: 3,
-    imagenes: '../imgs/7793742002922.jpg'
-}, {
-    idProducto: 3,
-    cantidad: 1,
-    precio: 5,
-    producto: 'Dermaglos  Protector solar 65',
-    descuento: false,
-    stock: 3,
-    imagenes: '../imgs/7793742003264.jpg'
-}];
+
+loadProductos = () => {
+    return fetch('/data/data.json')
+        .then(response => response.json())
+        .then((data) => {
+            return data;
+        });
+}
 var carrito = new Cart();
-addCarrito = (id) => {
+addCarrito = async (id) => {
     let domCart = document.querySelector("#cantidad");
     let element = '<span id="cantidad">@elemento</span>'
-    debugger
+    let productos;
+  await  loadProductos().then((response) => productos = response);
     let productoToCart = productos.filter((item) => item.idProducto == id);
     for (const producto of productoToCart) {
-        carrito.addCart(producto.idProducto, producto.cantidad, producto.precio, producto.producto, producto.descuento,producto.imagenes,producto.stock);
+        carrito.addCart(producto.idProducto, producto.cantidad, producto.precio, producto.producto, producto.descuento, producto.imagenes, producto.stock);
         console.log(`el producto fue agregado  ${producto.producto}`)
     }
 
@@ -149,6 +133,6 @@ deleteProducto = () => {
 // carrito.addQuantity(2);
 // console.log(carrito.carrito)
 
-window.onload = function() {
+window.onload = function () {
     AOS.init()
 }
